a87d2b71132ead19bace55fafcbe28705ceb468b
shubham nishad <66104219+iamTHOR2@users.noreply.github.com>
2024-07-21 10:19:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/facedetectionml/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.facedetectionml
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.facedetectionml.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnCamera.setOnClickListener{
+            
+        }
+    }
+}
