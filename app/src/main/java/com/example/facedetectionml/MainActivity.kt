e387f6fdb65d6f927c768d4da160b5ed4573accd
shubham nishad <66104219+iamTHOR2@users.noreply.github.com>
2024-07-24 14:15:47 +0530

    First commit
    Camera working fine needs to work on toast message for smile probability

--- app/src/main/java/com/example/facedetectionml/MainActivity.kt
+++ app/src/main/java/com/example/facedetectionml/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.facedetectionml
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Telephony.Mms.Intents
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.facedetectionml.databinding.ActivityMainBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,7 +34,61 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.btnCamera.setOnClickListener{
-            
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(intent.resolveActivity(packageManager) != null){
+                startActivityForResult(intent, 123)
+            }else{
+                Toast.makeText(this,"Oops someting went wrong", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode==123 && resultCode== RESULT_OK){
+            val extras = data?.extras
+            val bitmap = extras?.get("data") as? Bitmap
+            if (bitmap != null) {
+                detectFace(bitmap)
+            }
+        }
+    }
+
+    private fun detectFace(bitmap: Bitmap) {
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+            .build()
+
+        val detector = FaceDetection.getClient(options)
+        val image = InputImage.fromBitmap(bitmap, 0)
+
+        val result = detector.process(image)
+            .addOnSuccessListener { faces ->
+                // Task completed successfully. our face is successfully detected
+                var resultText = " "
+                var i = 1
+                for(face in faces){
+                    resultText = "Face Number : $i" +
+                            "\nSmile : ${face.smilingProbability?.times(100)}%" +
+                            "\nLeft Eye Open : ${face.leftEyeOpenProbability?.times(100)}%"+
+                            "\nRight Eye Open : ${face.rightEyeOpenProbability?.times(100)}%"
+                    i++
+                }
+
+                if(faces.isEmpty()){
+                    Toast.makeText(this,"Face not detected", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,resultText, Toast.LENGTH_LONG).show()
+                    Log.d("TAG" , resultText)
+                }
+            }
+            .addOnFailureListener { e ->
+                // Task failed with an exception, face detection is failed
+                Toast.makeText(this,"Something wrong", Toast.LENGTH_SHORT).show()
+            }
     }
 }
